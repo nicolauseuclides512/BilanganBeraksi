065553609bdb03f33435394c5dc741e4652d1081
nicoe <nicoe@DESKTOP-PS316TC>
2022-10-11 15:18:33 +0700

    Perubahan logika pada kelas Main BilanganBeraksi

--- BilanganBeraksi/src/main/java/com/arakhel/bilanganberaksi/BilanganBeraksi.java
+++ BilanganBeraksi/src/main/java/com/arakhel/bilanganberaksi/BilanganBeraksi.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.arakhel.bilanganberaksi;
 
 /**
@@ -12,6 +11,14 @@ package com.arakhel.bilanganberaksi;
 public class BilanganBeraksi {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Bilangan b1 = new Bilangan(50);
+        b1.info();
+
+        Bilangan b2 = new Bilangan(15);
+        b2.info();
+
+        Bilangan b3 = new Bilangan(30);
+        b3.info();
+
     }
 }
